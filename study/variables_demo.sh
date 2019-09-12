87c6bc481f523655a55fd8cc9c7ef096ee8a4b14
haoliwei <735878019@qq.com>
2019-09-12 11:54:13 +0800

    add file.sh

---
+++ study/variables_demo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#define path variables
+#by haoliwei
+
+A=123
+
+echo "print A variabls value: $A"
+
+echo "print PWD:  $PWD"
+echo "print UID:  $UID"
+
+
+echo "#########################"
+
+echo "print \$0 $0"
+echo "print \$1 $1"
